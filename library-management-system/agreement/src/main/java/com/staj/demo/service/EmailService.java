1d966d12f821b3ec06a331f90dc0a78b23b41025
Caner Coskun <canercoskun51@gmail.com>
2024-07-18 00:21:28 +0300

    EmailService yazildi.

---
+++ library-management-system/agreement/src/main/java/com/staj/demo/service/EmailService.java
@@ -0,0 +1,23 @@
+package com.staj.demo.service;
+
+import com.staj.demo.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class EmailService {
+    private JavaMailSender javaMailSender;
+    public void sendMail(User user) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setSubject("Library-Management-System");
+        mailMessage.setText(user.getEmail() +" adresi ile başarılı bir şekilde kayıt oldunuz.");
+        mailMessage.setFrom("canercoskun51@gmail.com");
+        javaMailSender.send(mailMessage);
+        System.out.println("Mail gönderildi");
+    }
+
+}
--- library-management-system/agreement/src/main/java/com/staj/demo/service/UserService.java
+++ library-management-system/agreement/src/main/java/com/staj/demo/service/UserService.java
@@ -12,25 +12,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-
     private UserRepository userRepository;
-    private JavaMailSender javaMailSender;
+   private EmailService emailService;
+
     public User addUser(User user) {
-        sendMail(user);
+        emailService.sendMail(user);
         return userRepository.save(user);
     }
 
 
 
-    private void sendMail(User user) {
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(user.getEmail());
-        mailMessage.setSubject("Library-Management-System");
-        mailMessage.setText(user.getEmail() +" adresi ile başarılı bir şekilde kayıt oldunuz.");
-        mailMessage.setFrom("canercoskun51@gmail.com");
 
-        javaMailSender.send(mailMessage);
-        System.out.println("Mail gönderildi");
-    }
 
 }
