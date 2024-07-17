fd5b1e5277ae5d51e6ce37c14a98eb836fbbf332
Caner Coskun <canercoskun51@gmail.com>
2024-07-17 21:10:34 +0300

    Kayıt olan kullaniciya mail gönderme fonksiyonu yazildi.

--- library-management-system/agreement/src/main/java/com/staj/demo/service/UserService.java
+++ library-management-system/agreement/src/main/java/com/staj/demo/service/UserService.java
@@ -4,6 +4,8 @@ package com.staj.demo.service;
 import com.staj.demo.model.User;
 import com.staj.demo.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
@@ -12,8 +14,23 @@ public class UserService {
 
 
     private UserRepository userRepository;
+    private JavaMailSender javaMailSender;
     public User addUser(User user) {
+        sendMail(user);
         return userRepository.save(user);
+    }
+
+
+
+    private void sendMail(User user) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setSubject("Library-Management-System");
+        mailMessage.setText(user.getEmail() +" adresi ile başarılı bir şekilde kayıt oldunuz.");
+        mailMessage.setFrom("canercoskun51@gmail.com");
+
+        javaMailSender.send(mailMessage);
+        System.out.println("Mail gönderildi");
     }
 
 }
