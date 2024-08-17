436b72cc1827ba30b2e71cdcc0da32741cc22ed6
Caner Coskun <canercoskun51@gmail.com>
2024-08-17 21:23:41 +0300

    config-server eklendi.

--- library-management-system/book-service/src/main/java/com/example/book_service/BookServiceApplication.java
+++ library-management-system/book-service/src/main/java/com/example/book_service/BookServiceApplication.java
@@ -2,6 +2,8 @@ package com.example.book_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 public class BookServiceApplication {
